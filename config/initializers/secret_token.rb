0c5643c94332eed1164e75bb1f6141f802d2292b
Bratislav Petrovic <bratislav.petrovic@gmail.com>
2014-10-20 12:20:20 +0200

    bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ggame::Application.config.secret_key_base = '65180af824320e2bf47673417d0fcf872b331ac8eb26104d5bdbd0b59d8940582f69b2483a6f7f1d4d99c57f701e8343bcd88f115f4d141f8b11ef34419e1830'
